1536262a104f88ac5bf50470130d0f4e448fb858
Mike Erb <merb92@gmail.com>
2020-02-15 16:26:37 -0500

    added additional comments for the write function

--- convert_sav_to_csv.r
+++ convert_sav_to_csv.r
@@ -1,4 +1,4 @@
 library(foreign)
 
-# SAV file
+# SAV file assumes entries are not quoted and the seperator is a comma
 write.table(read.spss("spss_data.sav"), file="from_sav_data.csv", quote = FALSE, sep = ",")
